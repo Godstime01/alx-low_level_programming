e12dbbbc7e2b24c5360a875a548d4839c6f2a204
godstime01 <youngestkid01@gmail.com>
2022-10-14 11:33:20 -0700

    task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * main - Entry point                                  *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	for (int i = 48; i <= 57; i++)
+	int i;
+
+	for (i = 48; i <= 57; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
