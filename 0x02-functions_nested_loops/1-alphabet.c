e936dc7398e3396e5d74988754f60c059c0ce4a6
godstime01 <youngestkid01@gmail.com>
2022-10-18 18:52:27 -0700

    doing task 2-3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,15 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Entry point
- * Return: 0 success
+ * print_alphabet - Entry point
  */
 
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 
 void print_alphabet(void)
 {
 	char i;
 
 	for (i = 97; i <= 122; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
+		_putchar(i);
+	_putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints a-z 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int counter = 1;
+	char l;
+
+	for (; counter <= 10; counter++)
+	{
+		for (l = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
