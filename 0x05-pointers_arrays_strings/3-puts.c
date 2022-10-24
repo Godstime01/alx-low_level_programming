48f0fe47699f0925f21934ad7d17b61be98674d8
godstime01 <youngestkid01@gmail.com>
2022-10-24 12:58:20 -0700

    task 3 -

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,20 @@
 #include "main.h"
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <string.h>
 
 /**
  * _puts - prints a string
- * @str: string parameter 
+ * @str: string parameter
  * Return: void
  */
 
 void _puts(char *str)
 {
 	int i = 0;
-	do
-	{
+
+	do {
 		_putchar(str[i]);
 		i++;
-	}
-	while(str[i] != "\o");
+	} while (str[i] != '\0');
 	_putchar("\n");
 }
