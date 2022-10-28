74e44c85ca5721e46b4ba766e745d2d32829901d
godstime01 <youngestkid01@gmail.com>
2022-10-28 16:51:55 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,10 +13,10 @@ char *string_toupper(char *str)
 
 	for (i = 0; i < strlen(str); i++)
 	{
-		if (str[i] == ' ' || str[i] == '\n')
-			continue;
 		if (str[i] >= 97 && str[i] =< 122)
 			str[i] -=  32;
+		else
+			continue;
 	}
 	return (str);
 }
