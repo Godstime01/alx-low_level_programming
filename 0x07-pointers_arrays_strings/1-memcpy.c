d1ff277d633b658cd03def3549ac5024011c170b
godstime01 <youngestkid01@gmail.com>
2022-10-31 13:34:09 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: copy destination
+ * @src: copy source
+ * @n: n byte
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; (dest[i] == src[i]) != '\0'; i++)
+	return (dest);
+}
